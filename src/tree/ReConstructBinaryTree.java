3c92ea5a85bee3c54704274f6c408fba6b10c1ee
xiaojun <lhyzxj314@qq.com>
2017-05-20 18:33:53 +0800

    重命名

--- src/tree/ReConstructBinaryTree.java
+++ src/tree/ReConstructBinaryTree.java
@@ -6,7 +6,7 @@ package tree;
  * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
  * @author xshrimp 2017年5月20日
  */
-public class Solution {
+public class ReConstructBinaryTree {
   public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
     if (pre == null || in == null)
       return null;

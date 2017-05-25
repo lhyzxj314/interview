a89cb36f3973892415dfba3ffed4ff1573470020
xj <lhyzxj314@qq.com>
2017-05-25 22:17:04 +0800

    变态青蛙跳

--- src/fibonacci_seq/SuperFrogJump.java
+++ src/fibonacci_seq/SuperFrogJump.java
@@ -3,8 +3,8 @@ package fibonacci_seq;
 import java.util.*;
 
 /**
- * 青蛙跳台阶(可抽象成求斐波那契数列) 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
- * 
+ * 变态青蛙跳
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
  * @author xshrimp 2017年5月25日
  */
 public class SuperFrogJump {
@@ -28,7 +28,7 @@ public class SuperFrogJump {
 
 	public static void main(String[] args) {
 		SuperFrogJump obj = new SuperFrogJump();
-		int res = obj.JumpFloorII(3);
+		int res = obj.JumpFloorII(31);
 		System.out.println(res);
 	}
 }

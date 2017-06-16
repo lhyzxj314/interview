b9553db2a866463e1dac5cd5b1f49900a1e04713
xj <lhyzxj314@qq.com>
2017-06-16 16:29:22 +0800

    剑指offer——字符串的全排列

--- src/string/Permutation.java
+++ src/string/Permutation.java
@@ -19,22 +19,21 @@ public class Permutation {
 		
 		StringBuffer sb = new StringBuffer(str);
 		ArrayList<String> res = new ArrayList<String>();
-		Set<String> temp = new HashSet<String>();
+		
 		for (int i = 0; i < sb.length(); i++) {
 			StringBuffer tempStr = new StringBuffer(str);
 			StringBuffer head = new StringBuffer(tempStr.substring(i, i + 1));
 			StringBuffer tail = new StringBuffer(tempStr.deleteCharAt(i));
-			ArrayList<StringBuffer> strsBuffer = permution(head, tail);
+			Set<StringBuffer> strsBuffer = permution(head, tail);
 			for (StringBuffer s : strsBuffer)
-				temp.add(s.toString());
+				res.add(s.toString());
 		}
-		res.addAll(temp);      // 去重
 		Collections.sort(res); // 按字典序排列
 		return res;
 	}
 	
-	public ArrayList<StringBuffer> permution(StringBuffer head, StringBuffer tail) {
-		ArrayList<StringBuffer> res = new ArrayList<StringBuffer>();
+	public Set<StringBuffer> permution(StringBuffer head, StringBuffer tail) {
+		Set<StringBuffer> res = new HashSet<StringBuffer>();
 		if (tail.length() == 0) {
 			res.add(head);
 			return res;
@@ -44,13 +43,12 @@ public class Permutation {
 			StringBuffer tempStr = new StringBuffer(tail);
 			StringBuffer tTead = new StringBuffer(tempStr.substring(i, i + 1));
 			StringBuffer tTail = new StringBuffer(tempStr.deleteCharAt(i));
-			ArrayList<StringBuffer> tails = permution(tTead, tTail);
+			Set<StringBuffer> tails = permution(tTead, tTail);
 			for (StringBuffer t : tails) {
 				StringBuffer h = new StringBuffer(head);
 				res.add(h.append(t));
 			}
 		}
-		
 		return res;
 	}
 	

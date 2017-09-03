4891d336ccd5708b8485160ccd09ebda6a7e1776
xiaojun <lhyzxj314@qq.com>
2017-09-03 14:51:36 +0800

    剑指offer——最大连续自序列 解法2

--- src/array/GreatestSumOfSubArray.java
+++ src/array/GreatestSumOfSubArray.java
@@ -1,11 +1,11 @@
 package array;
 
 public class GreatestSumOfSubArray {
-
+  
 	public int findGreatestSumOfSubArray1(int[] array) {
 		if (array == null || array.length == 0)
 			return 0;
-
+		
 		int maxSum = Integer.MIN_VALUE;
 		int tempSum = 0;
 		for (int i = 0; i < array.length; i++) {
@@ -19,4 +19,32 @@ public class GreatestSumOfSubArray {
 		}
 		return maxSum;
 	}
+	
+	public int findGreatestSumOfSubArray2(int[] array) {
+    if (array == null || array.length == 0)
+      return 0;
+    
+    int[] max = new int[array.length];
+    max[0] = array[0];
+    for (int i = 1; i < array.length; i++) {
+      if (max[i - 1] > 0)
+        max[i] = max[i - 1] + array[i];
+      else
+        max[i] = array[i];
+    }
+    
+    int res = Integer.MIN_VALUE;
+    for (Integer m : max)
+      res = Math.max(res, m);
+    return res;
+  }
+	
+	public static void main(String[] args) {
+	  GreatestSumOfSubArray obj = new GreatestSumOfSubArray();
+    int[] array = new int[] {-2,-8,-1,-5,-9};
+    int res1 = obj.findGreatestSumOfSubArray1(array);
+    System.out.println(res1);
+    int res2 = obj.findGreatestSumOfSubArray2(array);
+    System.out.println(res2);
+  }
 }
